c4b30fed50463f1186d4b18da1395ae57a012ec5
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-05-28 15:16:13 +0200

    pizza italiano slug parmegiano

---
+++ .history/src/lib/generatePage_20250528151507.js
@@ -0,0 +1,13 @@
+async function generatePage(searchTerm) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_LINK}/api/generatePage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ searchTerm }),
+    }
+  );
+  return response;
+}
---
+++ .history/src/lib/generatePage_20250528151512.js
@@ -0,0 +1,14 @@
+"use server";
+async function generatePage(searchTerm) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_LINK}/api/generatePage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ searchTerm }),
+    }
+  );
+  return response;
+}
